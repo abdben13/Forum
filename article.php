090364ba54ac3cf3567292cdd23261e2d9a720d8
abdel <abdelaziz.bensenouci2023@campus-eni.fr>
2024-02-02 14:06:01 +0100

    sauvegarde02/02  réponse à une question

--- article.php
+++ article.php
@@ -1,6 +1,8 @@
 <?php 
     session_start();
-    require('actions/questions/showArticleContentAction.php');?>
+    require('actions/questions/showArticleContentAction.php');
+    require('actions/questions/postAnswerAction.php');
+    ?>
 <!DOCTYPE html>
 <html lang="fr">
     <?php include 'includes/head.php'; ?>
@@ -14,15 +16,29 @@
         if(isset($errorMsg)){ echo $errorMsg;}
             if(isset($question_publication_date)){
               ?>
-              <h3><?= $question_title; ?> </h3>
-              <hr>
-              <p><?= $question_content; ?></p>
-              <hr>
-              <small><?= $question_pseudo_author. ' ' . $question_publication_date ?></small>
-              <?php
+            <section class="show-content">
+                <h3><?= $question_title; ?> </h3>
+                <hr>
+                <p><?= $question_content; ?></p>
+                <hr>
+                <small><?= $question_pseudo_author. ' ' . $question_publication_date ?></small>
+            </section>
+            <br>
+            <section class="show-answers">
+            <form class="form-group" method="POST">
+                    <div class="mb-3">
+                        <label for="exampleInputEmail1" class="form-label">Réponse :</label>
+                        <textarea name="answer" class="form-control"></textarea>
+                        <br>
+                        <button class="btn btn-primary" type="submit" name="validate">Répondre</button>
+                    </div>
+            </form>
+
+            </section>
+            <?php
             }
         ?>
-
+        
     </div>
 </body>
 </html>
